d024ca2708754844c726ed70de4f1435187bda27
Bernard Llanos <llanos@ualberta.ca>
2017-07-05 15:17:47 -0600

    Analyzing disparity with respect to any parameter in addition to depth.

--- ray_tracing/radialChromaticAberration.m
+++ ray_tracing/radialChromaticAberration.m
@@ -1,28 +1,28 @@
 function [ disparity_spline, disparity_raw ] = radialChromaticAberration(...
-    stats, reference_wavelength_index, z, reference_z, varargin...
+    stats, x_fields, reference_wavelength_index, z, reference_z, varargin...
 )
 % RADIALCHROMATICABERRATION  Model chromatic aberration
 %
 % ## Syntax
 % disparity_spline = radialChromaticAberration(...
-%     stats, reference_wavelength_index,...
+%     stats, x_fields, reference_wavelength_index,...
 %     z, reference_z [, wavelengths, wavelengths_to_rgb]...
 % )
 % [ disparity_spline, disparity ] = radialChromaticAberration(...
-%     stats, reference_wavelength_index,...
+%     stats, x_fields, reference_wavelength_index,...
 %     z, reference_z [, wavelengths, wavelengths_to_rgb]...
 % )
 %
 % ## Description
 % disparity_spline = radialChromaticAberration(...
-%     stats, reference_wavelength_index,...
+%     stats, x_fields, reference_wavelength_index,...
 %     z, reference_z [, wavelengths, wavelengths_to_rgb]...
 % )
 %   Returns a spline model of chromatic aberration as a function of
 %   distance from the origin, and scene depth.
 %
 % [ disparity_spline, disparity ] = radialChromaticAberration(...
-%     stats, reference_wavelength_index,...
+%     stats, x_fields, reference_wavelength_index,...
 %     z, reference_z [, wavelengths, wavelengths_to_rgb]...
 % )
 %   Additionally returns the disparity values used to build the spline
@@ -38,12 +38,9 @@ function [ disparity_spline, disparity_raw ] = radialChromaticAberration(...
 %   at the j-th depth.
 %
 %   `stats.(name)` can be a scalars or vectors, as long as all elements are
-%   the same size. Vectors will be converted to distances from the origin
-%   when producing the first input variable used for spline fitting. (The
-%   other input variable is the scene depth value.) The lengths of
-%   differences between vectors at the reference wavelength, and vectors at
-%   other wavelengths, will be used as the response variable values for
-%   spline fitting.
+%   the same size. The lengths of differences between vectors at the
+%   reference wavelength, and vectors at other wavelengths, will be used as
+%   the response variable values for spline fitting.
 %
 %   Consequently, this function can generate trends in a variety of
 %   quantities with respect to scene depth, such as image positions
@@ -51,6 +48,19 @@ function [ disparity_spline, disparity_raw ] = radialChromaticAberration(...
 %
 %   `stats` might be an array of the `stats` output argument of
 %   'analyzePSF()', for example.
+%
+% x_fields -- Spline fitting input field names
+%   A scalar structure with the same fields as `stats`, where each value is
+%   a two-element cell vector of strings. `x_fields.(name)` contains
+%   the fieldname in `stats` of the values to be used as one of the input
+%   variables for spline fitting for the values in `stats.(name)`.
+%
+%   The splines output in `disparity_spline.(name)` model disparities
+%   between the values in `stats.(name)` for different wavelengths. The
+%   splines are thin-plate splines in a 2D parameter space, where one
+%   parameter is depth, and the other parameter is the magnitudes
+%   (distances from the origin) of the values in
+%   `stats(:, reference_wavelength_index, :).(x_fields.(name))`.
 %
 % reference_wavelength_index -- Reference wavelength
 %   The index into the second dimension of `stats` representing the
@@ -93,16 +103,14 @@ function [ disparity_spline, disparity_raw ] = radialChromaticAberration(...
 %
 % disparity_spline -- Thin-plate spline models of chromatic aberration
 %   A set of thin-plate smoothing splines modeling chromatic aberration as
-%   a function of `z`, and `r`. In the case of image positions, `r` is the
-%   2D distance in the image plane, of an image point from the origin,
-%   where the image point was formed by light at the reference wavelength.
-%   More generally, `r` is the distance of a value in `stats.(name)` from
-%   the origin.
+%   a function of `z`, and `r`. `r` is the distance of a value in `stats(:,
+%   reference_wavelength_index, :).(x_fields.(name))` from the
+%   origin.
 %
-%   `disparity_spline.(name)` is a list of length `size(stats, 2)`, where
-%   the k-th cell contains a thin-plate smoothing spline describing the
-%   aberration between the k-th wavelength and the reference wavelength,
-%   for the statistic 'name'.
+%   `disparity_spline.(name)` is a cell vector of length `size(stats, 2)`,
+%   where the k-th cell contains a thin-plate smoothing spline describing
+%   the aberration between the k-th wavelength and the reference
+%   wavelength, for the statistic 'name'.
 %   `disparity_spline(reference_wavelength_index).(name)` is an empty cell.
 %
 %   `aberration = fnval(disparity_spline(k).(name),[r; z])` evaluates the
@@ -111,15 +119,15 @@ function [ disparity_spline, disparity_raw ] = radialChromaticAberration(...
 %   vector of distances between point spread function statistics of type
 %   'name'.
 %
-%   If there are too few data points to estimate spline models, all
-%   elements of `disparity_spline.(name)` are empty cells.
+%   If there are too few data points to estimate spline models, the
+%   corresponding elements of `disparity_spline.(name)` are empty cells.
 %
 % disparity_raw -- Input disparity values
 %   The disparity values, calculated from the values in `stats` and `z`,
 %   used to construct `disparity_spline`. `disparity_raw` has the same
 %   dimensions as `stats`; `disparity_raw(i, k, j)` is the displacement
 %   vector from `stats(i, reference_wavelength_index, j).(name)` to
-%   `stats(i, k, j).(name)`. This disparity vector is therefore measured
+%   `stats(i, k, j).(name)`. Therefore, this disparity vector is measured
 %   for the i-th scene feature, emitting light at the k-th wavelength, and
 %   positioned at the j-th depth.
 %
@@ -131,7 +139,7 @@ function [ disparity_spline, disparity_raw ] = radialChromaticAberration(...
 % File created June 27, 2017
 
 nargoutchk(1, 2);
-narginchk(4, 6);
+narginchk(5, 7);
 
 if ~isempty(varargin)
     if length(varargin) ~= 2
@@ -153,12 +161,19 @@ n_points = sz(1);
 n_wavelengths = sz(2);
 n_depths = sz(3);
 
+% Preliminary processing
 if verbose
     n_points_all_depths = n_points * n_depths;
-    disparity_z_plot = zeros(n_points_all_depths, 1);
+    zeros_plot = zeros(n_points_all_depths, 1);
 end
 
 stats_cell = struct2cell(stats);
+for i = 1:n_names
+    stats_cell_i = squeeze(stats_cell(i, :, :, :));
+    stats_cell_i = reshape(stats_cell_i, n_points, 1, n_wavelengths, n_depths);
+    stats_mat_i = cell2mat(stats_cell_i);
+    stats_mat.(names{i}) = stats_mat_i;
+end
 
 z_adjusted = z - reference_z;
 z_adjusted = repelem(z_adjusted, n_points);
@@ -168,78 +183,89 @@ end
 
 for i = 1:n_names
     name_display_i = replace(names{i}, '_', '\_');
-    stats_cell_i = squeeze(stats_cell(i, :, :, :));
-    stats_cell_i = reshape(stats_cell_i, n_points, 1, n_wavelengths, n_depths);
-    stats_mat_i = cell2mat(stats_cell_i);
     
+    % Find the other input variable
+    name_x = x_fields.(names{i});
+    name_display_x = replace(name_x, '_', '\_');
+
+    stats_mat_i = stats_mat.(names{i});
     stats_reference_i = repmat(...
         stats_mat_i(:, :, reference_wavelength_index, :),...
         1, 1, n_wavelengths, 1 ...
     );
     disparity_raw_i = stats_mat_i - stats_reference_i;
-    dimensionality = size(stats_mat_i, 2);
-
-    if verbose && (dimensionality == 2 || dimensionality == 1)
-        stats_mat_i_3D = reshape(permute(stats_mat_i, [1, 4, 2, 3]), [], dimensionality, n_wavelengths);
-        disparity_raw_i_3D = reshape(permute(disparity_raw_i, [1, 4, 2, 3]), [], dimensionality, n_wavelengths);
-
+    
+    stats_mat_x = stats_mat.(name_x);
+    
+    dimensionality_i = size(disparity_raw_i, 2);
+    dimensionality_x = size(stats_mat_x, 2);
+    
+    if verbose && dimensionality_i <= 2 && dimensionality_x <= 2
+        
+        stats_mat_x_3D = reshape(permute(stats_mat_x, [1, 4, 2, 3]), [], dimensionality_x, n_wavelengths);
+        disparity_raw_i_3D = reshape(permute(disparity_raw_i, [1, 4, 2, 3]), [], dimensionality_i, n_wavelengths);
+        
         figure
         hold on
         legend_strings = cell(n_wavelengths * 2 - 1, 1);
         for k = 1:n_wavelengths
-            if dimensionality == 2
-                scatter3(...
-                    stats_mat_i_3D(:, 1, k), stats_mat_i_3D(:, 2, k),...
-                    z_adjusted, [], wavelengths_to_rgb(k, :), 'o'...
-                );
-            elseif dimensionality == 1
-                scatter(...
-                    stats_mat_i_3D(:, 1, k),...
-                    z_adjusted, [], wavelengths_to_rgb(k, :), 'o'...
-                );
+            if dimensionality_x == 1
+                y_plot = zeros_plot;
+            else
+                y_plot = stats_mat_x_3D(:, 2, k);
             end
+            
+            scatter3(...
+                stats_mat_x_3D(:, 1, k), y_plot,...
+                z_adjusted, [], wavelengths_to_rgb(k, :), 'o'...
+            );
             legend_strings{k} = sprintf(...
-                'Values for \\lambda = %g nm',...
-                wavelengths(k)...
+                '''%s'' for \\lambda = %g nm',...
+                name_display_x, wavelengths(k)...
             );
         end
         k_legend = 1;
         for k = 1:n_wavelengths
             if k ~= reference_wavelength_index
-                if dimensionality == 2
-                    quiver3(...
-                        stats_mat_i_3D(:, 1, reference_wavelength_index),...
-                        stats_mat_i_3D(:, 2, reference_wavelength_index),...
-                        z_adjusted.',...
-                        disparity_raw_i_3D(:, 1, k),...
-                        disparity_raw_i_3D(:, 2, k),...
-                        disparity_z_plot,...
-                        'Color', wavelengths_to_rgb(k, :), 'AutoScale', 'off'...
-                    );
-                elseif dimensionality == 1
-                    quiver(...
-                        stats_mat_i_3D(:, 1, reference_wavelength_index),...
-                        z_adjusted.',...
-                        disparity_raw_i_3D(:, 1, k),...
-                        disparity_z_plot,...
-                        'Color', wavelengths_to_rgb(k, :), 'AutoScale', 'off'...
-                    );
+                if dimensionality_x == 1
+                    y_plot = zeros_plot;
+                else
+                    y_plot = stats_mat_x_3D(:, 2, reference_wavelength_index);
                 end
+                if dimensionality_i == 1
+                    v_plot = zeros_plot;
+                else
+                    v_plot = disparity_raw_i_3D(:, 2, k);
+                end
+            
+                quiver3(...
+                    stats_mat_x_3D(:, 1, reference_wavelength_index),...
+                    y_plot,...
+                    z_adjusted.',...
+                    disparity_raw_i_3D(:, 1, k),...
+                    v_plot,...
+                    zeros_plot,...
+                    'Color', wavelengths_to_rgb(k, :), 'AutoScale', 'off'...
+                );
                 legend_strings{n_wavelengths + k_legend} = sprintf(...
-                    'Aberration for \\lambda = %g nm', wavelengths(k)...
+                    'Aberration of ''%s'' for \\lambda = %g nm',...
+                    name_display_i, wavelengths(k)...
                 );
                 k_legend = k_legend + 1;
             end
         end
         legend(legend_strings);
-        title(sprintf('Values and aberrations for the ''%s'' statistic', name_display_i))
-        if dimensionality == 2
-            xlabel('Value dimension 1 (e.g. X)');
-            ylabel('Value dimension 2 (e.g. y)');
+        title(sprintf(...
+            'Aberrations of ''%s'' at values of ''%s''',...
+            name_display_i, name_display_x...
+        ))
+        if dimensionality_x == 2
+            xlabel(sprintf('%s_x', name_display_x));
+            ylabel(sprintf('%s_y', name_display_x));
             zlabel('Depth')
-        elseif dimensionality == 1
-            xlabel('Value');
-            ylabel('Depth')
+        elseif dimensionality_x == 1
+            xlabel(sprintf('%s', name_display_x));
+            zlabel('Depth')
         end
         hold off
     elseif verbose
@@ -250,15 +276,15 @@ for i = 1:n_names
     disparity_raw_i_radial_signs = sign(dot(stats_reference_i, disparity_raw_i, 2));
     disparity_raw_i_radial_signs(disparity_raw_i_radial_signs == 0) = 1;
     disparity_raw_i_radial = disparity_raw_i_radial .* disparity_raw_i_radial_signs;
-    stats_reference_i_radial = sqrt(dot(...
-        stats_mat_i(:, :, reference_wavelength_index, :),...
-        stats_mat_i(:, :, reference_wavelength_index, :),...
+    stats_reference_x_radial = sqrt(dot(...
+        stats_mat_x(:, :, reference_wavelength_index, :),...
+        stats_mat_x(:, :, reference_wavelength_index, :),...
         2 ...
     ));
-    stats_reference_i_radial = stats_reference_i_radial(:).';
+    stats_reference_x_radial = stats_reference_x_radial(:).';
 
     % Prepare data for spline fitting
-    spline_predictors = [ stats_reference_i_radial; z_adjusted ];
+    spline_predictors = [ stats_reference_x_radial; z_adjusted ];
     spline_responses = permute(squeeze(disparity_raw_i_radial), [1, 3, 2]);
     spline_responses = reshape(spline_responses, 1, [], n_wavelengths);
 
@@ -286,7 +312,10 @@ for i = 1:n_names
                     spline_responses_k...
                 );
             else
-                warning('Insufficient data points available to construct a spline model in radial position and depth for \\lambda = %g nm.', wavelengths(k))
+                warning(...
+                    'Insufficient data points available to construct a spline model of ''%s'' as a function of ''%s'', and depth, for \\lambda = %g nm.',...
+                    name_i, name_x, wavelengths(k)...
+                )
             end
 
             if verbose
@@ -296,23 +325,22 @@ for i = 1:n_names
                     surf(pts{1}, pts{2}, pts{3}, 'EdgeColor', 'none', 'FaceAlpha', 0.7);
                     colorbar
                     colormap summer
-                    c = colorbar;
-                    c.Label.String = 'Disparity';
+                    colorbar;
                     title(sprintf(...
-                        'Aberration in %s for \\lambda = %g nm',...
+                        'Aberration in ''%s'' for \\lambda = %g nm',...
                         name_display_i, wavelengths(k)...
                     ))
                     legend_str = {'Thin plate spline'};
                 else
                     title(sprintf(...
-                        'Aberration in %s for \\lambda = %g nm (Insufficient data for spline model)',...
+                        'Aberration in ''%s'' for \\lambda = %g nm (Insufficient data for spline model)',...
                         name_display_i, wavelengths(k)...
                     ))
                     legend_str = {};
                 end
-                xlabel('Distance from the origin');
+                xlabel(sprintf('Magnitude of ''%s''', name_display_x));
                 ylabel('Depth');
-                zlabel('Disparity');
+                zlabel(sprintf('Aberration in ''%s''', name_display_i));
                 hold on
                 plot3(...
                     spline_predictors_k(1, :), spline_predictors_k(2, :),...
