e1dbf4df57db707e84e0f86f8e29021ba77e31d3
Bernard Llanos <llanos@ualberta.ca>
2018-05-29 09:14:18 -0600

    Switched to forward difference gradients.
    - Tested image gradient matrix generation functions.

--- aberration_correction/spatialGradient.m
+++ aberration_correction/spatialGradient.m
@@ -31,12 +31,13 @@ function [ G_xy ] = spatialGradient(image_sampling)
 %
 % ## Algorithm
 %
-% The central difference formula is used to produce the image gradient. Boundary
-% pixels are replicated to allow the formula to be applied at the image boundaries.
+% The intermediate difference formula is used to produce the image
+% gradient. Boundary pixels are replicated to allow the formula to be
+% applied at the image boundaries.
 %
 % ## References
-% - The central difference formula for the image gradient is given in the MATLAB
-%   help page for the 'imgradientxy()' function.
+% - The intermediate difference formula for the image gradient is given in
+%   the MATLAB help page for the 'imgradientxy()' function.
 %   (https://www.mathworks.com/help/images/ref/imgradientxy.html)
 %
 % See also spectralGradient
@@ -56,63 +57,66 @@ end
 image_height = image_sampling(1);
 image_width = image_sampling(2);
 c = image_sampling(3);
-n_px = prod(image_sampling(1:2));
 n_px_c = prod(image_sampling);
-% Each element of the central difference gradient is calculated from two pixels
+% Each element of the intermediate difference gradient is calculated from
+% two pixels
 n_offsets = 2;
 % x-gradients, and y-gradients
 n_gradients = 2;
 
+% Create filters to enforce the boundary conditions
+% - Omit the last column for the x-gradient, and the last row for the
+%   y-gradient, as these pixels get zero values from the intermediate
+%   difference formula, according to the boundary conditions (pixel
+%   replication).
+nonzero_gx = sub2ind(...
+    image_sampling,...
+    repmat((1:image_height).', (image_width - 1) * c, 1),...
+    repmat(repelem((1:(image_width - 1)).', image_height), c, 1),...
+    repelem((1:c).', image_height * (image_width - 1))...
+    );
+nonzero_gy = sub2ind(...
+    image_sampling,...
+    repmat((1:(image_height - 1)).', image_width * c, 1),...
+    repmat(repelem((1:image_width).', (image_height - 1)), c, 1),...
+    repelem((1:c).', (image_height - 1) * image_width)...
+    );
+
 % Row indices
-% - Go through all pixels once per gradient direction
-% - Within each gradient direction, create two matrix elements per pixel
-rows = repmat(repelem((1:n_px_c).', n_offsets), n_gradients, 1);
+rows = repelem([
+    nonzero_gx;
+    nonzero_gy + n_px_c
+    ], n_offsets);
 
 % Column indices
-
-% Enumerate the positions of all pixels in the image
-[X, Y] = meshgrid(1:image_width, 1:image_height);
-x = repmat(X(:), c, 1);
-y = repmat(Y(:), c, 1);
-
-neighbour_x = zeros(n_px_c, n_offsets, n_gradients);
-neighbour_y = zeros(n_px_c, n_offsets, n_gradients);
-for g = 1:n_gradients
-    if g == 1
-        offsets = [-1 0; 1 0];
-    else
-        offsets = [0 -1; 0 1];
-    end
-    for i = 1:n_offsets
-        neighbour_x(:, i, g) = x + offsets(i, 1);
-        neighbour_y(:, i, g) = y + offsets(i, 2);
-    end
-end
-neighbour_x = neighbour_x(:);
-neighbour_y = neighbour_y(:);
-neighbour_c = repmat(repelem((1:c).', n_px), n_gradients, 1);
-
-% Replicate pixels outside the image boundaries
-neighbour_x(neighbour_x < 1) = 1;
-neighbour_x(neighbour_x > image_width) = image_width;
-neighbour_y(neighbour_y < 1) = 1;
-neighbour_y(neighbour_y > image_height) = image_height;
-
-% Convert to linear indices
-neighbour_index = sub2ind(...
+col_neighbour_gx = sub2ind(...
     image_sampling,...
-    neighbour_y,...
-    neighbour_x,...
-    neighbour_c...
-);
+    repmat((1:image_height).', (image_width - 1) * c, 1),...
+    repmat(repelem((2:image_width).', image_height), c, 1),...
+    repelem((1:c).', image_height * (image_width - 1))...
+    );
+col_neighbour_gy = sub2ind(...
+    image_sampling,...
+    repmat((2:image_height).', image_width * c, 1),...
+    repmat(repelem((1:image_width).', (image_height - 1)), c, 1),...
+    repelem((1:c).', (image_height - 1) * image_width)...
+    );
+
+n_px_x = length(col_neighbour_gx);
+n_px_y = length(col_neighbour_gy);
+cols = zeros((n_px_x + n_px_y) * n_offsets, 1);
+cols(1:2:(n_px_x * 2)) = nonzero_gx;
+cols(2:2:(n_px_x * 2)) = col_neighbour_gx;
+cols(((n_px_x * 2) + 1):2:end) = nonzero_gy;
+cols(((n_px_x * 2) + 2):2:end) = col_neighbour_gy;
 
 % Matrix values
-elements = repmat([-0.5; 0.5], n_px_c * n_gradients);
+elements = repmat([-1; 1], n_px_x + n_px_y, 1);
 
 % Assemble the sparse matrix
 G_xy = sparse(...
     rows,...
-    neighbour_index,...
+    cols,...
     elements,...
     n_px_c * n_gradients, n_px_c...
 );
--- aberration_correction/spectralGradient.m
+++ aberration_correction/spectralGradient.m
@@ -15,11 +15,10 @@ function [ G_lambda ] = spectralGradient(image_sampling, replicate)
 %   colour channels or wavelength bands, respectively, of the image.
 %
 % replicate -- Boundary conditions
-%   A Boolean value indicating how to treat the first and last colour
-%   channels (or wavelength bands) of the image. If `true`, the first and
-%   last colour channels will be replicated prior to taking the gradient.
-%   Otherwise, the gradient will not be calculated for the first and last
-%   colour channels.
+%   A Boolean value indicating how to treat the last colour channel (or
+%   wavelength band) of the image. If `true`, the last colour channel will
+%   be replicated prior to taking the gradient. Otherwise, the gradient
+%   will not be calculated for the last colour channel.
 %
 % ## Output Arguments
 %
@@ -36,14 +35,14 @@ function [ G_lambda ] = spectralGradient(image_sampling, replicate)
 %   wavelength bands, then `I` contains the data from the image in order
 %   first by row, then by column, then by colour channel. `c_out` is equal
 %   to `c` if `replicate` is `true`. Otherwise, `c_out` is equal to `c -
-%   2`.
+%   1`.
 %
 % ## Algorithm
 %
-% The central difference formula is used to produce the gradient. Boundary
+% The forward difference formula is used to produce the gradient. Boundary
 % colour channels are replicated to allow the formula to be applied to
 % every colour channel, if `replicate` is `true`. Otherwise, the gradient
-% is only calculated for colour channels with two adjacent colour channels.
+% is only calculated for colour channels with a following colour channel.
 %
 % See also spatialGradient
 
@@ -60,48 +59,40 @@ if length(image_sampling) ~= 3
 end
 
 c = image_sampling(3);
-if replicate
-    c_out = c;
-else
-    c_out = c - 2;
-end
-if c_out < 1
-    error('Cannot output less than one gradient channel.');
-end
+c_nonzero = c - 1;
 n_px = prod(image_sampling(1:2));
 n_px_c = prod(image_sampling);
-n_px_cout = n_px * c_out;
+n_px_c_nonzero = n_px * c_nonzero;
 
 % Row indices
 % - Go through all pixels once, and create two matrix elements per pixel
-offsets = [1; 3];
+offsets = [0; 1];
 n_offsets = size(offsets, 1);
-rows = repelem((1:n_px_cout).', n_offsets);
+rows = repelem((1:n_px_c_nonzero).', n_offsets);
 
 % Column indices
-% Each element of the central difference gradient is calculated from two colour
-% channel values
-channel_indices = repmat(offsets, n_px * (c - 2), 1) +...
-    repelem((0:(c - 3)).', n_px * n_offsets);
-if replicate
-    channel_indices = [
-        ones(n_px * n_offsets, 1);
-        channel_indices;
-        repmat(c, n_px * n_offsets, 1)
-    ];
-end
+% Each element of the forward difference gradient is calculated from two
+% colour channel values
+channel_indices = repmat(offsets, n_px_c_nonzero, 1) +...
+    repelem((1:c_nonzero).', n_px * n_offsets);
 
 % Convert to linear indices
-columns = repmat(repelem((1:n_px).', 2), c_out, 1) + (channel_indices - 1) * n_px;
+columns = repmat(repelem((1:n_px).', n_offsets), c_nonzero, 1) +...
+    (channel_indices - 1) * n_px;
 
 % Matrix values
-elements = repmat([-0.5; 0.5], n_px_cout);
+elements = repmat([-1; 1], n_px_c_nonzero, 1);
 
 % Assemble the sparse matrix
+if replicate
+    sz_1 = n_px_c;
+else
+    sz_1 = n_px_c_nonzero;
+end
 G_lambda = sparse(...
     rows,...
     columns,...
     elements,...
-    n_px_cout, n_px_c...
+    sz_1, n_px_c...
 );
 end
